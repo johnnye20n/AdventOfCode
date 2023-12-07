30bc25fad360aade6fea828babd96c3990ad5b66
Johnny Eriksson <johnny@e20n.se>
2023-12-07 11:17:16 +0100

    7a

---
+++ day5b/Program.cs
@@ -0,0 +1,167 @@
+﻿var input = File.ReadAllLines(@"C:\Code\advent\2023\AdventOfCode\day5b\input.txt");
+
+var seedToSoilMaps = new SeedMap("seed-to-soil");
+var soilToFertilizerMaps = new SeedMap("soil-to-fertilize");
+var fertilizerToWaterMaps = new SeedMap("fertilize-to-water");
+var waterToLightMaps = new SeedMap("water-to-light");
+var lightToTemperatureMaps = new SeedMap("light-to-temp");
+var temperatureToHumidityMaps = new SeedMap("temp-to-hum");
+var humidityToLocationMaps = new SeedMap("hum-to-loc");
+
+SeedCache.KnownEnds.Add("seed-to-soil", []);
+SeedCache.KnownEnds.Add("soil-to-fertilize", []);
+SeedCache.KnownEnds.Add("fertilize-to-water", []);
+SeedCache.KnownEnds.Add("water-to-light", []);
+SeedCache.KnownEnds.Add("light-to-temp", []);
+SeedCache.KnownEnds.Add("temp-to-hum", []);
+
+seedToSoilMaps.Next = soilToFertilizerMaps;
+soilToFertilizerMaps.Next = fertilizerToWaterMaps;
+fertilizerToWaterMaps.Next = waterToLightMaps;
+waterToLightMaps.Next = lightToTemperatureMaps;
+lightToTemperatureMaps.Next = temperatureToHumidityMaps;
+temperatureToHumidityMaps.Next = humidityToLocationMaps;
+
+var currentMap = "";
+
+for (var i = 1; i < input.Length - 1; i++)
+{
+    if (string.IsNullOrWhiteSpace(input[i]))
+    {
+        continue;
+    }
+
+    if (input[i].Contains("map"))
+    {
+        currentMap = input[i].Trim();
+        continue;
+    }
+
+    var split = input[i].Split(" ").Select(long.Parse).ToList();
+    var seedMap = new SeedMapItem
+    {
+        DestRangeStart = split[0],
+        SourceRangeStart = split[1],
+        RangeLength = split[2]
+    };
+
+    switch (currentMap)
+    {
+        case "seed-to-soil map:":
+            {
+                seedToSoilMaps.Add(seedMap);
+                break;
+            }
+        case "soil-to-fertilizer map:":
+            {
+                soilToFertilizerMaps.Add(seedMap);
+                break;
+            }
+        case "fertilizer-to-water map:":
+            {
+                fertilizerToWaterMaps.Add(seedMap);
+                break;
+            }
+        case "water-to-light map:":
+            {
+                waterToLightMaps.Add(seedMap);
+                break;
+            }
+        case "light-to-temperature map:":
+            {
+                lightToTemperatureMaps.Add(seedMap);
+                break;
+            }
+        case "temperature-to-humidity map:":
+            {
+                temperatureToHumidityMaps.Add(seedMap);
+                break;
+            }
+        case "humidity-to-location map:":
+            {
+                humidityToLocationMaps.Add(seedMap);
+                break;
+            }
+        default: throw new Exception();
+    }
+}
+
+var seeds = input[0].Split(":")[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+
+var lowest = (long)-1;
+
+for (var seed = 0; seed < seeds.Count; seed += 2)
+{
+    //79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93
+    var startrange = seeds[seed];
+    var endrange = startrange + seeds[seed + 1];
+    for (var i = startrange; i < endrange; i++)
+    {
+        var res = seedToSoilMaps.Find(seed, i);
+        if (res < 0)
+        {
+            continue;
+        }
+        lowest = lowest > -1 ? Math.Min(res, lowest) : res;
+        Console.WriteLine($"{i} / {endrange}");
+    }
+    Console.WriteLine($"seed {seed}/{seeds.Count} completed");
+}
+
+Console.WriteLine(lowest);
+
+class SeedMap
+{
+    public string Name { get; }
+    public SeedMap(string name)
+    {
+        Name = name;
+    }
+    public List<SeedMapItem> Items { get; set; } = [];
+
+    public void Add(SeedMapItem item)
+    {
+        Items.Add(item);
+    }
+
+    public SeedMap? Next { get; set; }
+    public bool IsFinal => Next == null;
+
+    public long Find(int seed, long val)
+    {        
+        var item = Items.FirstOrDefault(x => val >= x.SourceRangeStart && val < x.SourceRangeStart + x.RangeLength);
+        if (item == null)
+        {
+            //Console.WriteLine($"{Name} => next {val}");
+            var nullres = IsFinal ? val : Next!.Find(seed, val);
+            return nullres;
+        }
+
+        //Console.WriteLine($"{Name} => next {item.DestRangeStart + (val - item.SourceRangeStart)}");
+        var res = IsFinal ? item.DestRangeStart + (val - item.SourceRangeStart) : Next!.Find(seed, item.DestRangeStart + (val - item.SourceRangeStart));
+        // if (item.IsChecked.ContainsKey(seed))
+        // {
+        //     return item.IsChecked[seed];
+        // }
+        // item.IsChecked.Add(seed, res);
+        return res;
+    }
+
+}
+
+static class SeedCache
+{
+    public static Dictionary<string, Dictionary<long, long>> KnownEnds { get; set; } = [];
+}
+class SeedMapItem
+{
+    public long DestRangeStart { get; set; }
+    public long SourceRangeStart { get; set; }
+    public long RangeLength { get; set; }
+    public Dictionary<int, long> IsChecked { get; set; } = [];
+    public long SourceRangeEnd => SourceRangeStart + RangeLength;
+    public bool IsInRange(long val)
+    {
+        return val >= SourceRangeStart && val < SourceRangeEnd;
+    }
+}
---
+++ day7a/Program.cs
@@ -0,0 +1,144 @@
+﻿var input = File.ReadAllLines(@"C:\Code\advent\2023\AdventOfCode\day7a\input.txt");
+var draws = new List<Draw>();
+
+var i = 0;
+foreach (var line in input)
+{
+    var split = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    var draw = new Draw(i, split[0], int.Parse(split[1]));
+    draws.Add(draw);
+    i++;
+}
+
+var groupedDraws = draws.GroupBy(x => (int)x.Hand).Select(x => x).ToList();
+
+foreach (var group in groupedDraws)
+{
+    foreach (var item in group)
+    {
+        var others = group.Where(x => x.Id != item.Id).ToList();
+        item.Rank = others.Count(item.IsWinner) + 1;
+    }
+}
+
+var currentSubRank = 0;
+var winnings = 0;
+foreach (var group in groupedDraws.OrderBy(x => x.Key))
+{
+    foreach (var item in group)
+    {
+        winnings += item.Winnings(currentSubRank);
+    }
+    currentSubRank += group.Max(x => x.Rank);
+}
+
+Console.WriteLine(winnings);
+
+
+class Draw
+{
+    public int Id { get; }
+    public int Rank { get; set; }
+
+    public int Winnings(int subrank) => (subrank + Rank) * Bid;
+    public Draw(int id, string cards, int bid)
+    {
+        Id = id;
+        Cards = cards;
+        Bid = bid;
+        var cardArray = cards.ToCharArray();
+        if (IsXOfKind(cardArray, 5))
+        {
+            Hand = Hand.FiveOfAKind;
+        }
+        else if (IsXOfKind(cardArray, 4))
+        {
+            Hand = Hand.FourOfAKind;
+        }
+        else if (IsXOfKind(cardArray, 3) && IsXOfKind(cardArray, 2))
+        {
+            Hand = Hand.FullHouse;
+        }
+        else if (IsXOfKind(cardArray, 3))
+        {
+            Hand = Hand.ThreeOfAKind;
+        }
+        else if (cardArray.GroupBy(x => x).Select(x => x).Where(y => y.Count() == 2).Count() == 2)
+        {
+            Hand = Hand.TwoPair;
+        }
+        else if (IsXOfKind(cardArray, 2))
+        {
+            Hand = Hand.OnePair;
+        }
+        else
+        {
+            Hand = Hand.HighCard;
+        }
+    }
+
+    public string Cards { get; }
+    public int Bid { get; }
+    public Hand Hand { get; }
+    static bool IsXOfKind(char[] cardArray, int num)
+    {
+        return cardArray.GroupBy(x => x).Select(x => x).Any(y => y.Count() == num);
+    }
+
+    public bool IsWinner(Draw draw)
+    {
+        if ((int)Hand > (int)draw.Hand)
+        {
+            return true;
+        }
+        if ((int)Hand < (int)draw.Hand)
+        {
+            return false;
+        }
+
+        for (var i = 0; i < Cards.Length; i++)
+        {
+            if (CardValueMap[Cards[i]] > CardValueMap[draw.Cards[i]])
+            {
+                return true;
+            }
+
+            if (CardValueMap[Cards[i]] < CardValueMap[draw.Cards[i]])
+            {
+                return false;
+            }
+        }
+        throw new Exception("fel");
+    }
+
+    private Dictionary<char, int> CardValueMap { get; set; } = new Dictionary<char, int> {
+        { 'A', 14 },
+        { 'K', 13 },
+        { 'Q', 12 },
+        { 'J', 11 },
+        { 'T', 10 },
+        { '9', 9 },
+        { '8', 8 },
+        { '7', 7 },
+        { '6', 6 },
+        { '5', 5 },
+        { '4', 4 },
+        { '3', 3 },
+        { '2', 2 },
+        { '1', 1 }
+    };
+}
+
+
+enum Hand
+{
+    FiveOfAKind = 10,
+    FourOfAKind = 9,
+    FullHouse = 8,
+    ThreeOfAKind = 7,
+    TwoPair = 6,
+    OnePair = 5,
+    HighCard = 4
+}
+
