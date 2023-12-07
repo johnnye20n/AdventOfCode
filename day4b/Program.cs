14092c127803f16dac1cb562c97f62e1b634bb9a
Johnny Eriksson <johnny@e20n.se>
2023-12-07 19:26:39 +0100

    4b

--- day4b/Program.cs
+++ day4b/Program.cs
@@ -1,29 +1,39 @@
-﻿internal class Program
+﻿
+internal class Program
 {
-    private static int total = 0;
-    private static List<string> output = new();
+    private static Dictionary<int, List<int>> RowWinners = [];
+    private static Dictionary<int, int> RowWinnersSum = [];
     private static void Main(string[] args)
     {
-        var input = File.ReadAllLines(@"C:\Code\advent\2023\AdventOfCode\day4b\input2.txt");
-        total = 0;//input.Length;
+        var input = File.ReadAllLines(@"C:\Code\advent\2023\AdventOfCode\day4b\input.txt");
         //Card   1:  8 86 59 90 68 52 55 24 37 69 | 10 55  8 86  6 62 69 68 59 37 91 90 24 22 78 61 58 89 52 96 95 94 13 36 81
-        
+
         CheckCards(input, 0, input.Length - 1);
-        output = output.OrderBy(x => x).ToList();
-        foreach(var o in output) {
-            Console.WriteLine(o);
+
+        foreach (var row in RowWinners.OrderByDescending(x => x.Key).ToList())
+        {
+            var currentRowSum = row.Value.Count;
+
+            foreach (var winner in row.Value)
+            {
+                if (RowWinnersSum.ContainsKey(winner))
+                {
+                    currentRowSum += RowWinnersSum[winner];
+                }
+            }
+            RowWinnersSum.Add(row.Key, currentRowSum);
         }
-        Console.WriteLine(total);
+        Console.WriteLine(RowWinnersSum.Sum(x => x.Value) + input.Length);
     }
-    private static void CheckCards(string[] input, int startCheck, int endCheck, int level = 0)
+    private static void CheckCards(string[] input, int startCheck, int endCheck)
     {
         for (var i = startCheck; i < endCheck; i++)
-        {            
-            if (i > input.Length - 1)
+        {
+            if (!RowWinners.ContainsKey(i))
             {
-                break;
+                RowWinners.Add(i, []);
             }
-            Console.WriteLine($"line {i+1} checked");
+
             var split = input[i].Split(":");
             var winnersplit = split[1].Split("|");
 
@@ -31,15 +41,13 @@
             var numbers = winnersplit[1].Chunk(3).Select(x => int.Parse(string.Join("", x).Trim())).ToList();
 
             var correct = numbers.Where(x => winners.Contains(x)).Count();
-            total += Math.Max(correct, 1);
-            output.Add($"level {level} card {i+1} winners {i+2}-{i + 1 + correct}");
-            //line 1 3 correct => check 2,3,4
-            if (correct > 0)
+            Console.WriteLine($"row {i} wins {correct} copies");
+
+            for (var j = 0; j < correct; j++)
             {
-                
-                CheckCards(input, i + 1, i + correct, level+1);
+                RowWinners[i].Add(j + i + 1);
+                Console.WriteLine($"adding {j + i + 1} to dictionary");
             }
         }
     }
-
 }
