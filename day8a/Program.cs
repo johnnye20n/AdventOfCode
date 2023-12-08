ec9d14ff8a719c883ceff84cdaf76019314cb523
Johnny Eriksson <johnny@e20n.se>
2023-12-08 09:48:23 +0100

    8a

---
+++ day8a/Program.cs
@@ -0,0 +1,45 @@
+﻿var input = File.ReadAllLines(@"C:\Code\advent\2023\AdventOfCode\day8a\input.txt");
+
+var instructions = input[0].ToCharArray();
+
+var map = new Dictionary<string, Dictionary<string, string>>();
+
+for (var i = 2; i < input.Length; i++)
+{
+    var splitted = input[i].Split("=", StringSplitOptions.TrimEntries);
+    var directions = splitted[1].Split(",", StringSplitOptions.TrimEntries);
+    map.Add(splitted[0], new Dictionary<string, string> { { directions[0].Replace("(", ""), directions[1].Replace(")", "") } });
+}
+
+
+Console.WriteLine(instructions);
+foreach (var kvp in map)
+{
+    Console.WriteLine($"{kvp.Key} {kvp.Value.Select(y => y.Key).First()} {kvp.Value.Select(y => y.Value).First()}");
+}
+
+
+var steps = 0;
+var current = map.First(x => x.Key == "AAA");
+var found = false;
+
+do
+{
+    foreach (var instruction in instructions)
+    {
+        steps++;
+
+        var next = instruction == 'L' ? current.Value.Select(y => y.Key).First() : current.Value.Select(y => y.Value).First();
+        
+        if (next == "ZZZ")
+        {
+            found = true;
+            break;
+        }
+
+        current = map.First(x => x.Key == next);
+
+    }
+} while (found == false);
+
+Console.WriteLine(steps);
